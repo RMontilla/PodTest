b559bd6e8dddd415f12ac57a3400cee755cfbd01
Rafael <r.montilla.a@gmail.com>
2020-06-01 17:48:48 +0900

    Project Init
    Initializing project

---
+++ PodTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodTest
+//
+//  Created by rafaerum on 2020/06/01.
+//  Copyright © 2020 rafaerum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
